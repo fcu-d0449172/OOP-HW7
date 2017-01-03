7d3236aebca43cf4974c37e806b18eea9cb3f070
ASUS <ASUS@X555LF>
2017-01-03 16:13:52 +0800

    oop-hw7

--- src/fcu/iecs/oop/pokemon/Pokemon.java
+++ src/fcu/iecs/oop/pokemon/Pokemon.java
@@ -1,5 +1,29 @@
 package fcu.iecs.oop.pokemon;
 
-public class Pokemon {
-
+public abstract class Pokemon {
+	private final String name;
+	private final PokemonType type;
+	private int cp;
+	
+	public Pokemon(String name,PokemonType type,int cp){
+		this.name=name;
+		this.type=type;
+		this.cp=cp;
+	}
+	public abstract void attack();
+	
+	public String getName() {
+		return name;
+	}
+	public PokemonType getType() {
+		return type;
+	}
+	public int getCp() {
+		return cp;
+	}
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+	
+	
 }
